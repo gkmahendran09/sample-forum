5f7bc305627ee33c28bf295ceda188e422ebd7e0
Mahendran GK <gkm09@hotmail.com>
2018-01-12 21:25:42 +0530

    Episode 25 completed.

---
+++ app/RecordsActivity.php
@@ -0,0 +1,41 @@
+<?php
+namespace App;
+
+
+trait RecordsActivity {
+
+	protected static function bootRecordsActivity()
+	{
+		if(auth()->guest()) return;
+		
+		foreach(static::getActivitiesToRecord() as $event) {
+			static::$event(function($model) use ($event) {
+				$model->recordActivity($event);
+			});
+		}
+	}
+
+	protected static function getActivitiesToRecord()
+	{
+		return ['created'];
+	}
+
+	protected function recordActivity( $event ) {
+
+		$this->activity()->create([
+			'type'         => $this->getActivityType( $event ),
+			'user_id'      => auth()->id()
+		]);
+	}
+
+	protected function activity()
+	{
+		return $this->morphMany(Activity::class, 'subject');
+	}
+
+	protected function getActivityType( $event ) {
+		$type = strtolower( ( new \ReflectionClass( $this ) )->getShortName() );
+
+		return "{$event}_{$type}";
+	}
+}
