52807d41c6cf473426186eca3df64c906ff50fff
Mahendran GK <gkm09@hotmail.com>
2018-01-13 21:37:21 +0530

    Episode 26 completed

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,10 +9,21 @@ class ProfilesController extends Controller
 {
     public function show( User $user)
     {
-    	return view('profiles.show', [
+	    return view('profiles.show', [
     		'profileUser' => $user,
-		    'threads' => $user->threads()->paginate(20)
+		    'activities' => $this->getActivity( $user )
 	    ]);
     }
+
+	/**
+	 * @param User $user
+	 *
+	 * @return static
+	 */
+	protected function getActivity( User $user ) {
+		return $user->activity()->latest()->with( 'subject' )->get()->take( 50 )->groupBy( function ( $activity ) {
+			return $activity->created_at->format( 'Y-m-d' );
+		} );
+	}
 
 }
