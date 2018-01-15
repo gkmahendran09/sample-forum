6e8f9cec718f10924c5d5db89f3402d139c0ec42
Mahendran GK <gkm09@hotmail.com>
2018-01-15 17:19:23 +0530

    Episode 39 completed.

---
+++ resources/assets/js/mixins/collection.js
@@ -0,0 +1,20 @@
+export default {
+    data() {
+      return {
+          items: []
+      }
+    },
+    methods: {
+        add(items) {
+            this.items.push(items)
+
+            this.$emit('added')
+        },
+
+        remove(index) {
+            this.items.splice(index, 1)
+
+            this.$emit('removed')
+        }
+    }
+}
