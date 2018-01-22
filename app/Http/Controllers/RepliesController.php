a0b865d5ff22ac77c817409c20f57055e3d89626
Mahendran GK <gkm09@hotmail.com>
2018-01-22 18:44:39 +0530

    Episode 55 completed.

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Reply;
 use App\Thread;
 use Illuminate\Support\Facades\Gate;
@@ -29,27 +30,15 @@ class RepliesController extends Controller
 	 * @param Thread $thread
 	 *
 	 *
+	 * @param CreatePostRequest $form
+	 *
 	 * @return $this|\Illuminate\Http\RedirectResponse
 	 */
-	public function store($channelId, Thread $thread) {
-
-		if(Gate::denies('create', new Reply)) {
-			return response('You are posting too frequently. Please take a break. :)', 422);
-		}
-
-		try {
-			$this->validate(request(), ['body' => 'required|spamfree']);
-
-			$reply = $thread->addReply([
-				'body' => request('body'),
-				'user_id' => auth()->id()
-			]);
-
-		} catch(\Exception $e) {
-			return response('Sorry, your reply could not be saved at this time.', 422);
-		}
-
-		return $reply->load('owner');
+	public function store($channelId, Thread $thread, CreatePostRequest $form) {
+		return $thread->addReply([
+			'body' => request('body'),
+			'user_id' => auth()->id()
+		])->load('owner');
     }
 
 	/**
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -38,14 +38,15 @@ class ParticipateInForumTest extends TestCase
 
     /** @test */
     public function a_reply_requires_a_body() {
-	    $this->withExceptionHandling()->signIn();
+	    $this->withExceptionHandling();
+	    $this->signIn();
 
 	    // And an existing thread
 	    $thread = create( 'App\Thread' );
 
 	    // When the user adds a reply to the thread
 	    $reply = make( 'App\Reply', [ 'body' => null ] );
-	    $this->post( $thread->path() . '/replies', $reply->toArray() )
+	    $this->json('post', $thread->path() . '/replies', $reply->toArray() )
 		    ->assertStatus(422);
 //	         ->assertSessionHasErrors( 'body' );
 
@@ -105,6 +106,7 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function  replies_that_contain_spam_may_not_be_created()
     {
+    	    $this->withExceptionHandling();
 	    // Given we have a authenticated user
 	    $this->be($user = create('App\User'));
 
@@ -116,7 +118,7 @@ class ParticipateInForumTest extends TestCase
 	    	'body' => 'Yahoo Customer Support'
 	    ]);
 
-	    $this->post($thread->path() . '/replies', $reply->toArray())
+	    $this->json('post',$thread->path() . '/replies', $reply->toArray())
 	        ->assertStatus(422);
     
     }
@@ -124,6 +126,7 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function  users_may_only_reply_a_maximum_of_once_per_minute()
     {
+    	    $this->withExceptionHandling();
 	    $this->be($user = create('App\User'));
 
 	    // And an existing thread
@@ -138,7 +141,7 @@ class ParticipateInForumTest extends TestCase
 	         ->assertStatus(200);
 
 	    $this->post($thread->path() . '/replies', $reply->toArray())
-	         ->assertStatus(422);
+	         ->assertStatus(429);
     }
     
     
